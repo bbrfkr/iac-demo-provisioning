9eb644b85fb128f6536c58f6a1fd64bf92b9c7a3
EC2 Default User <ec2-user@ip-192-168-100-50.ap-northeast-1.compute.internal>
2018-01-13 17:33:37 +0000

    wip

--- create-envs.sh
+++ create-envs.sh
@@ -1,3 +1,4 @@
 ansible-playbook -i hosts playbooks/create-instances.yaml
 ./ec2.py --refresh-cache > /dev/null
 ansible-playbook -i ec2.py --private-key ~/bbrfkr-keypair-for-aws.pem playbooks/wait-for-instances-up.yaml
+ansible-playbook -i ec2.py --private-key ~/bbrfkr-keypair-for-aws.pem playbooks/deploy-instances.yaml
